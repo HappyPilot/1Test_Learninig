4058c8a20e6fa23dbef0bca7c21974046e0b5d2b
HappyPilot <6141026@gmail.com>
2021-03-06 09:11:21 +0100

    next step

--- src/Snake/js/snake.js
+++ src/Snake/js/snake.js
@@ -1,2 +1,6 @@
 var table = document.getElementById("box");
-console.log(table);
+    var fig1 = box.getContext("2d");
+        fig1.strokeStyle = "rgb(255,0,0)";
+        fig1.fillStyle =  "rgb(0,255,0)";
+        fig1.strokeRect(100,100,100,100);
+        fig1.fillRect(700,30,30,30);
